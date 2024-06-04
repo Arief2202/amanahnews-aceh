a9ea9bceadd6a04dba900a965e9585b16fe125d3
Arief <arief.d2202@gmail.com>
2024-06-04 12:26:16 +0700

    fix namespace

--- app/Models/postcontent.php
+++ app/Models/postcontent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostContent extends Model
+class postcontent extends Model
 {
     use HasFactory;
 }
--- app/Models/tagname.php
+++ app/Models/tagname.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tagname extends Model
+class tagname extends Model
 {
     use HasFactory;
 }
