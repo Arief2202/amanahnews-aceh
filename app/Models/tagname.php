33157120508efed38547cfef2a8b0de55b940e64
Arief <arief.d2202@gmail.com>
2024-06-04 12:04:51 +0700

    fix namespace

--- app/Models/tagname.php
+++ app/Models/tagname.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tagname extends Model
+class Tagname extends Model
 {
     use HasFactory;
 }
