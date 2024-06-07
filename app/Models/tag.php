3c040d390c89b03da9ff7dc4918023b74302f57a
Arief <arief.d2202@gmail.com>
2024-06-07 20:37:55 +0700

    berita done

--- app/Models/tag.php
+++ app/Models/tag.php
@@ -13,7 +13,7 @@ class tag extends Model
     ];
     
     public function post(){
-        return $this->belongsTo(post::class, 'post_id', 'id');
+        return $this->belongsTo(Post::class, 'post_id', 'id');
     }
     public function tagname(){
         return $this->belongsTo(tagname::class);
