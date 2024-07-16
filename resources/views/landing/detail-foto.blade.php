d1c5aaf8df27bead69815f790d1ad4995e68d723
Waridun <waridunnafis@gmail.com>
2024-07-16 11:56:18 +0700

    share fix

--- resources/views/landing/detail-foto.blade.php
+++ resources/views/landing/detail-foto.blade.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
   // $post = [
   //   'title' => fake()->sentence(8),
   //   'slug' => fake()->slug(),
   //   'category' => 'Teknologi',
   // ];
-  // $post = (object)($post); 
+  // $post = (object)($post);
   // $iklan = [];
   // $others = [];
   // $hots = [];
@@ -22,7 +22,7 @@
   <meta property="og:url" content="https://amanahnews.id"/>
   {{-- <meta property="og:image:width" content="500" />
   <meta property="og:image:height" content="500"/> --}}
-  <meta property="og:type" content="article"/> 
+  <meta property="og:type" content="article"/>
   <meta property="image_src" href="https://amanahnews.id/uploads/foto/image/{{$post->banner}}"/>
 @endsection
 
@@ -32,7 +32,7 @@
 @endsection
 
 @section('main')
-    <section class="mb-0 pb-0" style="margin-top: 40px;">
+    <section class="pb-0 mb-0" style="margin-top: 40px;">
         <div class="mb-0 page-title" data-aos="fade">
             <nav class="breadcrumbs">
               <div class="container">
@@ -46,21 +46,21 @@
           </div><!-- End Page Title -->
     </section>
 
-    <section class="mt-0 pt-0">
+    <section class="pt-0 mt-0">
         <div class="container p-0 mt-0">
 
-                <div class="ps-3 pe-3">                
+                <div class="ps-3 pe-3">
                     <div class="mt-3 mb-2">
                         <h1 style="font-weight:600">{{$post->title}}</h1>
                     </div>
                     <div class="mb-3 row">
-                        <div class="col-md-6 row mb-2">
+                        <div class="mb-2 col-md-6 row">
                             <div class="col-auto">
                                 <img src="/uploads/user/{{$post->user->photo}}" alt="" style="width:70px; height:70px; border-radius:50px">
                             </div>
                             <div class="col-6">
                                 <h5 class="mt-2" style="font-weight:600">{{$post->user->name}}</h5>
-                                <p class=" " style="font-weight:600; color:rgba(0, 0, 0, 0.486)">{{$post->user->instance}}</p>
+                                <p class="" style="font-weight:600; color:rgba(0, 0, 0, 0.486)">{{$post->user->instance}}</p>
                             </div>
                             <div>
                               <?php
@@ -79,16 +79,16 @@
                                   else{
                                       $valPrint = $val;
                                   }
-                                  
+
                               ?>
                                 <p>{{date('d M Y H:i', strtotime($post->updated_at))}} WIB - Total Views {{$post->view_total}}</p>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
 
-                <div class="row p-0 m-0">
+                <div class="p-0 m-0 row">
                   <div class="col-xl-8" style="">
                     <div class="swiper">
                       <script type="application/json" class="swiper-config">
@@ -108,8 +108,8 @@
                       </script>
                       <div class="swiper-wrapper">
 
-                        <div class="swiper-slide mb-3">
-                          <div class="testimonial-item p-2">
+                        <div class="mb-3 swiper-slide">
+                          <div class="p-2 testimonial-item">
                               <img src="/uploads/foto/image/{{$post->banner}}" alt="" style="width:100%">
                               <p class="mt-2" style="font-size:12px; color:rgba(0, 0, 0, 0.4)">{{$post->banner_source}}</p>
                               <div>
@@ -117,10 +117,10 @@
                               </div>
                           </div>
                         </div><!-- End testimonial item -->
-      
+
                         @foreach($post->contents->where('post_type', 'foto') as $postc)
-                        <div class="swiper-slide mb-3">
-                          <div class="testimonial-item p-2">
+                        <div class="mb-3 swiper-slide">
+                          <div class="p-2 testimonial-item">
                               @if($postc->href)<a href="{{$postc->href}}">@endif
                               <img src="/uploads/foto/image/{{$postc->content}}" alt="" style="max-width:{{$postc->image_width}}px; max-height:{{$postc->image_height}}px;">
                               @if($postc->href)</a>@endif
@@ -129,34 +129,34 @@
                           </div>
                         </div><!-- End testimonial item -->
                         @endforeach
-      
+
                       </div>
                       <div class="swiper-pagination"></div>
                     </div>
 
-                    <div class="row p-0 m-0 mt-4">
+                    <div class="p-0 m-0 mt-4 row">
                       <div class="col-auto">
                           <h4>Tag</h4>
                       </div>
                       @foreach($post->tags->where('post_type', 'foto') as $tag)
-                        <div class="col-auto p-0 m-0 me-2 mb-3">
+                        <div class="col-auto p-0 m-0 mb-3 me-2">
                             <a href="/foto/tag/{{$tag->tagname->slug}}" class="btn btn-primary-orange">{{$tag->tagname->name}}</a>
                         </div>
                       @endforeach
-                    </div>    
+                    </div>
                     <div class="mt-3" style="border-bottom:2px solid #000000;">
 
                     </div>
-                    <div class="mb-3 mt-2">
+                    <div class="mt-2 mb-3">
                           <h5 style="font-weight: 700;">Bagikan foto</h5>
-                          <a href="https://www.facebook.com/sharer/sharer.php?u={{route('berita.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-facebook" style="position: absolute; font-size: 30px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                          <a href="https://twitter.com/share?url={{route('berita.detail', ['slug' => $post->slug])}}" class="btn btn-dark me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-twitter-x" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                          <a href="https://www.linkedin.com/uas/login?session_redirect={{route('berita.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-linkedin" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                          <a href="https://web.whatsapp.com/send?text={{route('berita.detail', ['slug' => $post->slug])}}" class="btn btn-success me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-whatsapp" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                          <a href="https://telegram.me/share/url?url={{route('berita.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px; background-color:#26a5e4; border-color:#26a5e4"><i class="bi bi-telegram" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                    </div>                    
+                          <a href="https://www.facebook.com/sharer/sharer.php?u={{route('foto.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-facebook" style="position: absolute; font-size: 30px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
+                          <a href="https://twitter.com/share?url={{route('foto.detail', ['slug' => $post->slug])}}" class="btn btn-dark me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-twitter-x" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
+                          <a href="https://www.linkedin.com/uas/login?session_redirect={{route('foto.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-linkedin" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
+                          <a href="https://web.whatsapp.com/send?text={{route('foto.detail', ['slug' => $post->slug])}}" class="btn btn-success me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-whatsapp" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
+                          <a href="https://telegram.me/share/url?url={{route('foto.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px; background-color:#26a5e4; border-color:#26a5e4"><i class="bi bi-telegram" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
+                    </div>
                 </div>
-                <div class="col-xl-4 p-3">
+                <div class="p-3 col-xl-4">
                   @if($iklan)
                   <div class="mb-5">
                     <form action="{{route('iklan.click')}}" method="POST">@csrf
@@ -173,17 +173,17 @@
                       <div class="w-100" data-aos="fade-left" data-aos-delay="100">
                         <a href="/foto/detail/{{$hot->slug}}">
                             <h5 class="p-0 m-0" style="font-weight:700; font-size:16px;">{{$hot->title}}</h5>
-                            <div class="row p-0 m-0 mt-2">
+                            <div class="p-0 m-0 mt-2 row">
                                 <div class="col-auto p-0 m-0">
                                   <img src="/uploads/user/{{$post->user->photo}}" alt="" style="width:30px; height:30px; border-radius:50px">
                                 </div>
                                 <div class="col-auto p-0 m-0">
                                     <h5 style="font-weight:600; font-size:14px; margin-left:10px; margin-top:8px">{{$hot->user->name}}</h5>
                                 </div>
-                                <div class="col-auto ps-1 m-0 ms-2" style="padding:3px;width:4px; height:4px;">
+                                <div class="col-auto m-0 ps-1 ms-2" style="padding:3px;width:4px; height:4px;">
                                     <button class="btn btn-secondary" style="width:3px; height:4px; border-radius:50px; margin:0px; padding:0px;background-color:rgba(0, 0, 0, 0.2); border-color:transparent;"></button>
                                 </div>
-                                <div class="col p-0 m-0 ms-2">
+                                <div class="p-0 m-0 col ms-2">
                                     <p style="font-weight:600; font-size:12px; margin-top:7px; color:rgba(0, 0, 0, 0.423);">{{$hot->updated_at}}</p>
                                 </div>
                             </div>
@@ -197,13 +197,13 @@
     </section>
 
     <section class="section" style="padding-top:0px;">
-      <div class="container">        
+      <div class="container">
         <div class="container section-title" data-aos="fade-up">
           <h2>foto Lainnya</h2>
         </div>
         <div class="row">
           @foreach($others as $i=>$lainnya)
-          <div class="col-xl-4 mb-3">
+          <div class="mb-3 col-xl-4">
             <a href="{{route('foto.detail', ['slug' => $lainnya->slug])}}">
               <div class="" style="width:100%;" data-aos="flip-left" data-aos-delay="{{($i%3)*100}}">
                 <img src="/uploads/post/image/{{$lainnya->banner}}" alt="" style="max-height:300px;width: 100%">
@@ -216,7 +216,7 @@
           {{-- <div class="d-flex justify-content-center">
 
             <nav aria-label="...">
-                
+
                 <?php $per5 = (int)($others->currentPage()/3);?>
                 <ul class="pagination">
                   <li class="page-item @if($others->currentPage() <= 1) disabled @endif">
@@ -231,8 +231,8 @@
                         @endif
                     @endfor
                     <li class="page-item"><a class="page-link" href="{{route('foto', ['page'=>$per5*3+4])}}">{{$per5*3+4}}</a></li>
-                    
-                  @elseif($others->currentPage() > $others->lastPage()-3)                      
+
+                  @elseif($others->currentPage() > $others->lastPage()-3)
                     <li class="page-item"><a class="page-link" href="{{route('foto', ['page'=>$per5*3-4])}}">{{$per5*3-4}}</a></li>
                     @for($a=$others->lastPage()-3; $a<=$others->lastPage(); $a++)
                         @if($a == $others->currentPage())
@@ -241,7 +241,7 @@
                             <li class="page-item"><a class="page-link" href="{{route('foto', ['page'=>$a])}}">{{$a}}</a></li>
                         @endif
                     @endfor
-                  @else                 
+                  @else
                     <li class="page-item"><a class="page-link" href="{{route('foto', ['page'=>$per5*3-1])}}">{{$per5*3-1}}</a></li>
                     @for($a = ($per5 * 3); $a < ($per5 * 3 + 3); $a++)
                         @if($a == $others->currentPage())
--- resources/views/landing/detail-video.blade.php
+++ resources/views/landing/detail-video.blade.php
@@ -17,12 +17,12 @@
   <meta property="og:url" content="https://amanahnews.id"/>
   {{-- <meta property="og:image:width" content="500" />
   <meta property="og:image:height" content="500"/> --}}
-  <meta property="og:type" content="article"/> 
+  <meta property="og:type" content="article"/>
   <meta property="image_src" href="https://amanahnews.id/uploads/video/image/{{$post->banner}}"/>
 @endsection
 
 @section('main')
-    <section class="mb-0 pb-0" style="margin-top: 40px;">
+    <section class="pb-0 mb-0" style="margin-top: 40px;">
         <div class="mb-0 page-title" data-aos="fade">
             <nav class="breadcrumbs">
               <div class="container">
@@ -36,7 +36,7 @@
           </div><!-- End Page Title -->
     </section>
 
-    <section class="mt-0 pt-0">
+    <section class="pt-0 mt-0">
         {{-- <div class="col-1 " style="position: fixed; left:200px; top:200px;">
             <div class="row">
                 <div class="col-1">
@@ -49,18 +49,18 @@
             </div>
         </div> --}}
         <div class="container p-0 mt-0">
-            <div class="ps-3">                
+            <div class="ps-3">
                 <div class="mt-3 mb-2">
                     <h1 style="font-weight:600">{{$post->title}}</h1>
                 </div>
                 <div class="mb-3">
-                    <div class="row mb-2">
+                    <div class="mb-2 row">
                         <div class="col-auto">
                           <img src="/uploads/user/{{$post->user->photo}}" alt="" style="width:70px; height:70px; border-radius:50px">
                         </div>
                         <div class="col">
                             <h5 class="mt-2" style="font-weight:600">{{$post->user->name}}</h5>
-                            <p class=" " style="font-weight:600; color:rgba(0, 0, 0, 0.486)">{{$post->user->instance}}</p>
+                            <p class="" style="font-weight:600; color:rgba(0, 0, 0, 0.486)">{{$post->user->instance}}</p>
                         </div>
                     </div>
                     <div>
@@ -80,13 +80,13 @@
                               else{
                                   $valPrint = $val;
                               }
-                              
+
                           ?>
                           <p>{{date('d M Y H:i', strtotime($post->updated_at))}} WIB - Total Views {{$valPrint}}</p>
                     </div>
                 </div>
             </div>
-            <div class="row p-0 m-0">
+            <div class="p-0 m-0 row">
                 <div class="col-xl-8" style="">
                   <iframe style="width:100%; height:500px;" src="https://www.youtube.com/embed/{{$post->video}}"></iframe>
                     <p class="mt-2" style="font-size:12px; color:rgba(0, 0, 0, 0.4)">{{$post->video_source}}</p>
@@ -94,7 +94,7 @@
                       <?=$post->content ?>
                       @foreach($post->contents->where('post_type', 'video')->where('saved', '1') as $content)
                         @if($content->type=='image')
-                        <div class="row p-0 m-0">
+                        <div class="p-0 m-0 row">
                             {{-- <div class="col-xl-8" style=""> --}}
                                 @if($content->href)<a href="{{$content->href}}">@endif
                                 <img src="/uploads/post/image/{{$content->content}}" alt="" style="max-width:{{$content->image_width}}px; max-height:{{$content->image_height}}px;">
@@ -112,30 +112,30 @@
 
                       @endforeach
                     </div>
-                    
-                    <div class="row p-0 m-0 mt-4">
+
+                    <div class="p-0 m-0 mt-4 row">
                       <div class="col-auto">
                           <h4>Tag</h4>
                       </div>
                       @foreach($post->tags->where('post_type', 'video') as $tag)
-                        <div class="col-auto p-0 m-0 me-2 mb-3">
-                            <a href="/berita/tag/{{$tag->tagname->slug}}" class="btn btn-primary-orange">{{$tag->tagname->name}}</a>
+                        <div class="col-auto p-0 m-0 mb-3 me-2">
+                            <a href="/video/tag/{{$tag->tagname->slug}}" class="btn btn-primary-orange">{{$tag->tagname->name}}</a>
                         </div>
                       @endforeach
-                    </div>    
+                    </div>
                     <div class="mt-3" style="border-bottom:2px solid #000000;">
 
                     </div>
-                    <div class="mb-3 mt-2">
-                          <h5 style="font-weight: 700;">Bagikan Berita</h5>
+                    <div class="mt-2 mb-3">
+                          <h5 style="font-weight: 700;">Bagikan Video</h5>
                           <a href="https://www.facebook.com/sharer/sharer.php?u={{route('video.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-facebook" style="position: absolute; font-size: 30px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
                           <a href="https://twitter.com/share?url={{route('video.detail', ['slug' => $post->slug])}}" class="btn btn-dark me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-twitter-x" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
                           <a href="https://www.linkedin.com/uas/login?session_redirect={{route('video.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-linkedin" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
                           <a href="https://web.whatsapp.com/send?text={{route('video.detail', ['slug' => $post->slug])}}" class="btn btn-success me-2" style="position:relative; width:40px; height:40px; border-radius:5px"><i class="bi bi-whatsapp" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
                           <a href="https://telegram.me/share/url?url={{route('video.detail', ['slug' => $post->slug])}}" class="btn btn-primary me-2" style="position:relative; width:40px; height:40px; border-radius:5px; background-color:#26a5e4; border-color:#26a5e4"><i class="bi bi-telegram" style="position: absolute; font-size: 25px; transform:translate(-50%, -50%); top:49%; left:50%;"></i></a>
-                    </div>    
+                    </div>
                 </div>
-                <div class="col-xl-4 p-3">
+                <div class="p-3 col-xl-4">
                   @if($iklan)
                     <div class="mb-5">
                       <form action="{{route('iklan.click')}}" method="POST">@csrf
@@ -152,17 +152,17 @@
                       <div class="w-100" data-aos="fade-left" data-aos-delay="100">
                         <a href="/video/detail/{{$hot->slug}}">
                             <h5 class="p-0 m-0" style="font-weight:700; font-size:16px;">{{$hot->title}}</h5>
-                            <div class="row p-0 m-0 mt-2">
+                            <div class="p-0 m-0 mt-2 row">
                                 <div class="col-auto p-0 m-0">
                                   <img src="/uploads/user/{{$post->user->photo}}" alt="" style="width:30px; height:30px; border-radius:50px">
                                 </div>
                                 <div class="col-auto p-0 m-0">
                                     <h5 style="font-weight:600; font-size:14px; margin-left:10px; margin-top:8px">{{$hot->user->name}}</h5>
                                 </div>
-                                <div class="col-auto ps-1 m-0 ms-2" style="padding:3px;width:4px; height:4px;">
+                                <div class="col-auto m-0 ps-1 ms-2" style="padding:3px;width:4px; height:4px;">
                                     <button class="btn btn-secondary" style="width:3px; height:4px; border-radius:50px; margin:0px; padding:0px;background-color:rgba(0, 0, 0, 0.2); border-color:transparent;"></button>
                                 </div>
-                                <div class="col p-0 m-0 ms-2">
+                                <div class="p-0 m-0 col ms-2">
                                     <p style="font-weight:600; font-size:12px; margin-top:7px; color:rgba(0, 0, 0, 0.423);">{{$hot->updated_at}}</p>
                                 </div>
                             </div>
@@ -175,13 +175,13 @@
         </div>
     </section>
     <section class="section" style="padding-top:0px;">
-      <div class="container">        
+      <div class="container">
         <div class="container section-title" data-aos="fade-up">
-            <h2>Berita Video Lainnya</h2>
+            <h2>Video Lainnya</h2>
         </div>
         <div class="row">
           @foreach($others as $i=>$lainnya)
-          <div class="col-xl-4 mb-3">
+          <div class="mb-3 col-xl-4">
             <a href="{{route('video.detail', ['slug' => $lainnya->slug])}}">
               <div class="" style="width:100%;" data-aos="flip-left" data-aos-delay="{{($i%3)*100}}">
                 <img src="/uploads/video/image/{{$lainnya->banner}}" alt="" style="max-height:300px;width: 100%">
@@ -194,7 +194,7 @@
           <div class="d-flex justify-content-center">
 
             <nav aria-label="...">
-                
+
                 <?php $per5 = (int)($others->currentPage()/3);?>
                 <ul class="pagination">
                   <li class="page-item @if($others->currentPage() <= 1) disabled @endif">
@@ -209,8 +209,8 @@
                         @endif
                     @endfor
                     <li class="page-item"><a class="page-link" href="{{route('video', ['page'=>$per5*3+4])}}">{{$per5*3+4}}</a></li>
-                    
-                  @elseif($others->currentPage() > $others->lastPage()-3)                      
+
+                  @elseif($others->currentPage() > $others->lastPage()-3)
                     <li class="page-item"><a class="page-link" href="{{route('video', ['page'=>$per5*3-4])}}">{{$per5*3-4}}</a></li>
                     @for($a=$others->lastPage()-3; $a<=$others->lastPage(); $a++)
                         @if($a == $others->currentPage())
@@ -219,7 +219,7 @@
                             <li class="page-item"><a class="page-link" href="{{route('video', ['page'=>$a])}}">{{$a}}</a></li>
                         @endif
                     @endfor
-                  @else                 
+                  @else
                     <li class="page-item"><a class="page-link" href="{{route('video', ['page'=>$per5*3-1])}}">{{$per5*3-1}}</a></li>
                     @for($a = ($per5 * 3); $a < ($per5 * 3 + 3); $a++)
                         @if($a == $others->currentPage())
